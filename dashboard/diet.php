7f042e8422882871696288604ed42766c01c00a6
Adarsh Hegde <adhegde001@gmail.com>
2022-09-27 19:52:06 +0530

    test fonts

--- dashboard/diet.php
+++ dashboard/diet.php
@@ -6,9 +6,7 @@ include "../auth/check_logged_in.php";
 
 <head>
     <title>Diet</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;600;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
 
